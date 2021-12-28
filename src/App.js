98fd41708f1dca10b83dcc6a1b22be249ee86f68
bachtung144 <bachtung144007@gmail.com>
2021-12-28 10:10:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {Login} from "./screens/login";
+import { BrowserRouter , Route, Switch } from 'react-router-dom';
+import {Building} from "./screens/buildings";
+import NavigationBar from "./Navigation";
+import {Test} from "./screens/test";
+import {Apartment} from "./screens/apartments";
+import {Room} from "./screens/rooms";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/" component={Login} />
+                  <div>
+                      <NavigationBar/>
+                      <Route exact path="/buildings" component={Building}/>
+                      <Route path="/test" component={Test}/>
+                      <Route path="/buildings/:idApartment" component={Apartment}/>
+                      {/*<Route path="/buildings/apartments/rooms" component={Room}/>*/}
+                  </div>
+              </Switch>
+          </BrowserRouter>
+      </div>
   );
 }
 
